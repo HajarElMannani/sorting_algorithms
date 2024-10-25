0e4a15b8fb809bd85404726a49b41bab67ed8d64
HajarElMannani <laso125@hotmail.com>
2024-10-25 05:43:28 -0700

    betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,48 +1,37 @@
 #include "sort.h"
 
 /**
- *1-insertion_sort_list.c - function that sorts a doubly linked list of integers in ascending order using the Insertion sort algorithm
+ *insertion_sort_list - function that sorts a doubly linked list
+ * of integers in ascending order using the Insertion sort algorithm
  *@list: list to sort
  *Return: Nothing
  */
 void insertion_sort_list(listint_t **list)
 {
-  listint_t *temp, *curr;
-  listint_t *prev_node, *next_node;
-  if (list == NULL || *list == NULL)
-    return;
-  curr = (*list)->next;
+listint_t *temp, *curr;
+listint_t *prevn, *nextn;
+if (list == NULL || *list == NULL)
+return;
+curr = (*list)->next;
 while (curr != NULL)
-    {
-        temp = curr;
-        while (temp->prev != NULL && temp->n < temp->prev->n)
-        {
-           
-           prev_node = temp->prev;
-           next_node = temp->next;
-
-           
-            if (prev_node->prev)
-                prev_node->prev->next = temp;
-            temp->prev = prev_node->prev;
-
-           
-            if (next_node)
-                next_node->prev = prev_node;
-            prev_node->next = next_node;
-
-           
-            temp->next = prev_node;
-            prev_node->prev = temp;
-
-           
-            if (!temp->prev)
-                *list = temp;
-
-           
-            print_list(*list);
-        }
-       
-        curr = curr->next;
-    }
+{
+temp = curr;
+while (temp->prev != NULL && temp->n < temp->prev->n)
+{
+prevn = temp->prev;
+nextn = temp->next;
+if (prevn->prev != NULL)
+prevn->prev->next = temp;
+temp->prev = prevn->prev;
+if (nextn != NULL)
+nextn->prev = prevn;
+prevn->next = nextn;
+temp->next = prevn;
+prevn->prev = temp;
+if (temp->prev == NULL)
+*list = temp;
+print_list(*list);
+}
+curr = curr->next;
+}
 }
