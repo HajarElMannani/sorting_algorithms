ee78cc13bac4149ce1f847ebe10fc07128de03ba
HajarElMannani <laso125@hotmail.com>
2024-10-25 07:20:50 -0700

    update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,25 +9,26 @@
  */
 void bubble_sort(int *array, size_t size)
 {
+int swapd;
 int c = 0;
 size_t i, j;
 if (!array || size <= 1)
 return;
 for (i = 0; i < size - 1; i++)
 {
+swapd = 0;
 for (j = 0; j < size - i - 1; j++)
 {
-if (array[j] < array[j + 1])
-{
-continue;
-}
-else
+if (array[j] > array[j + 1])
 {
 c = array[j];
 array[j] = array[j + 1];
 array[j + 1] = c;
+swapd = 1;
+}
 }
 print_array(array, size);
-}
+if (!swapd)
+break;
 }
 }
