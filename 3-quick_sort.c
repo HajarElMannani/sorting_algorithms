227fed2d20fe48c9c92b74c1619f510ed767c6cd
HajarElMannani <laso125@hotmail.com>
2024-10-28 10:17:33 -0700

    fix errors

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -7,30 +7,37 @@
  *@size: size of the array
  *Return: index of pivot
  */
-size_t lomuto(int *array, int low_idx, int high_idx, size_t size)
+int lomuto(int *array, int low_idx, int high_idx, size_t size)
 {
 int i, j;
 int pivot;
 int c;
-i = low_idx - 1;
+i = low_idx;
 pivot = array[high_idx];
 for (j = low_idx; j < high_idx; j++)
 {
-if (array[j] <= pivot)
+if (array[j] < pivot)
 {
-i++;
+if (i < j)
+{
 c = array[i];
 array[i] = array[j];
 array[j] = c;
-}
+print_array(array, size);
 }
 i++;
+}
+}
+if (array[i] > pivot)
+{
 c = array[i];
 array[i] = array[high_idx];
 array[high_idx] = c;
 print_array(array, size);
+}
 return (i);
 }
+
 /**
  *func_sort - function that helps to check sort the array
  *@array: rray to check sort
@@ -41,7 +48,7 @@ return (i);
  */
 void func_sort(int *array, int low_idx, int high_idx, size_t size)
 {
-size_t i;
+int i;
 if (low_idx < high_idx)
 {
 i = lomuto(array, low_idx, high_idx, size);
